2a3212498cd06bc0ba1e213e11a94b6f84443487
rlcosta177 <rlcosta177@gmail.com>
2024-06-21 14:29:20 +0000

    workings on the hours

--- k8-deployment-files/run-kube.sh
+++ k8-deployment-files/run-kube.sh
@@ -2,17 +2,17 @@
 
 kubectl apply -f plik-deployment.yaml
 
-sleep 3
+sleep 2
 kubectl apply -f wiki-deployment.yaml
 
-sleep 3
+sleep 2
 kubectl apply -f secret-deployment.yaml
 
-sleep 3
+sleep 2
 kubectl apply -f ejbca-deployment.yaml
 
-sleep 3
+sleep 2
 kubectl apply -f nginx-configmap.yaml
 
-sleep 3
+sleep 2
 kubectl apply -f nginx-deployment.yaml
