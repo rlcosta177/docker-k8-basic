2d5e9c9f24aad860564b2ed5a970f4a59ba6091b
Rodrigo Costa <rlcosta177@gmail.com>
2024-07-18 09:58:01 +0000

    Update run-kube.sh

--- k8-deployment-files/run-kube.sh
+++ k8-deployment-files/run-kube.sh
@@ -31,6 +31,12 @@ sleep 2
 kubectl apply -f nginx-deployment.yaml
 
 
-# Wait for the NGINX deployment to be ready
-#kubectl rollout status deployment/nginx-proxy
+# Example of container start with rollout
+
+# Apply the One-time Secret deployment YAML
+#kubectl apply -f secret-deployment.yaml
+
+# Wait for the One-time Secret deployment to be ready
+#kubectl rollout status deployment/secret-service
+
 # Can be done with any deployment ofc
